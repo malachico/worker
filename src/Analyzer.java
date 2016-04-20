739e57407342dd8a5e48421777ed1185cf765f62
malachico <malachicohen@gmail.com>
2016-04-20 07:57:56 +0300

    added files

---
+++ src/Analyzer.java
@@ -0,0 +1,182 @@
+import com.amazonaws.services.sqs.model.DeleteMessageRequest;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.rnn.RNNCoreAnnotations;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.util.CoreMap;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * A worker process resides on an EC2 node. Its life cycle is as follows:
+ * <p>
+ * Repeatedly:
+ * Get a message from an SQS queue.
+ * Perform the requested job, and return the result.
+ * remove the processed message from the SQS queue.
+ */
+public class Analyzer {
+    private Analyzer() throws IOException {
+        Utils.init();
+    }
+
+    private String getTweet(String link) throws IOException {
+        // Get the page and parse it.
+        Document doc = Jsoup.connect(link).get();
+        // Extract page title.
+        String tweet = doc.select("title").toString();
+        // Clean string.
+        return tweet.substring(7, tweet.length() - 8);
+    }
+
+    /**
+     * Given a message of link, delete it from queue
+     *
+     * @param link link to delete
+     */
+    private void deleteLinkFromQueue(Message link) {
+        Utils.sqs_client.deleteMessage(new DeleteMessageRequest(Utils.manager_workers_queue_url, link.getReceiptHandle()));
+    }
+
+
+    /**
+     * Request a new link from the queue handler.
+     *
+     * @return Link to parse.
+     */
+    private Message getLinkFromSqs() {
+        ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(Utils.manager_workers_queue_url);
+        List<Message> messages = Utils.sqs_client.receiveMessage(receiveMessageRequest).getMessages();
+
+        if (messages.size() == 0) {
+            // Queue is empty.
+            return null;
+        }
+
+        // Get the first message in the queue.
+        return messages.get(0);
+    }
+
+    /**
+     * Process the data from the tweet using CoreNLP.
+     *
+     * @param tweet String to process.
+     * @return A sentiment score.
+     */
+    private int findSentiment(String tweet) {
+        Properties props = new Properties();
+        props.put("annotators", "tokenize, ssplit, parse, sentiment");
+        StanfordCoreNLP sentimentPipeline = new StanfordCoreNLP(props);
+
+        int mainSentiment = 0;
+        if (tweet != null && tweet.length() > 0) {
+            int longest = 0;
+            Annotation annotation = sentimentPipeline.process(tweet);
+
+            for (CoreMap sentence : annotation
+                    .get(CoreAnnotations.SentencesAnnotation.class)) {
+                Tree tree = sentence
+                        .get(SentimentCoreAnnotations.AnnotatedTree.class);
+                int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
+                String partText = sentence.toString();
+
+                if (partText.length() > longest) {
+                    mainSentiment = sentiment;
+                    longest = partText.length();
+                }
+            }
+        }
+
+        return mainSentiment;
+    }
+
+    private List printEntities(String tweet) {
+        List<String> entities = new ArrayList<String>();
+
+        Properties props = new Properties();
+        props.put("annotators", "tokenize , ssplit, pos, lemma, ner");
+        StanfordCoreNLP NERPipeline = new StanfordCoreNLP(props);
+
+        // create an empty Annotation just with the given text.
+        Annotation document = new Annotation(tweet);
+
+        // Run all Annotators on this text.
+        NERPipeline.annotate(document);
+
+        // These are all the sentences in this document.
+        // A CoreMap is essentially a Map that uses class objects as keys and has values with custom types.
+        List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+
+        for (CoreMap sentence : sentences) {
+            // Traversing the words in the current sentence.
+            // A CoreLabel is a CoreMap with additional token-specific methods.
+            for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+                // Text of the token.
+                String word = token.get(TextAnnotation.class);
+                // NER label of the token.
+                String named_entity = token.get(NamedEntityTagAnnotation.class);
+                if (named_entity.equals("PERSON") || named_entity.equals("LOCATION") || named_entity.equals("ORGANIZATION")) {
+                    entities.add(word + ":" + named_entity);
+                }
+            }
+        }
+
+        return entities;
+    }
+
+    private void putAnswerInQueue(int sentiment, List entities, String key) {
+        // Insert message to the queue.
+        Utils.sqs_client.sendMessage(new SendMessageRequest(Utils.workers_manager_queue_url, key + "|" + sentiment + "|" + entities.toString()));
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // Init analyzer.
+        Analyzer analyzer = new Analyzer();
+        while (true) {
+            // Get link to analyze from SQS.
+            Message message = analyzer.getLinkFromSqs();
+
+            // if queue is empty for next message..
+            if (message == null) {
+                Thread.sleep(500);
+                continue;
+            }
+
+            // Delete link from queue.
+            analyzer.deleteLinkFromQueue(message);
+
+            // Extract tweet from link.
+            String key_link = message.getBody();
+            System.out.println("body : " + key_link);
+
+            String key = key_link.split("\\|")[0];
+            String link = key_link.split("\\|")[1];
+
+            String tweet = analyzer.getTweet(link);
+
+            int sentiment = analyzer.findSentiment(tweet);
+            List entities = analyzer.printEntities(tweet);
+
+            System.out.println("sentiment : " + sentiment);
+            System.out.println("entities : " + entities);
+
+            // Insert answer to answers queue.
+            analyzer.putAnswerInQueue(sentiment, entities, key);
+        }
+    }
+}
---
+++ src/Utils.java
@@ -0,0 +1,183 @@
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.*;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.model.CreateQueueRequest;
+import com.amazonaws.services.sqs.model.PurgeQueueRequest;
+import org.apache.commons.codec.binary.Base64;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by malachi on 4/9/16.
+ *
+ * Common Amazon objects for local app, manager and workers for saving code
+ */
+class Utils {
+
+    static AmazonEC2 ec2_client;
+    static String manager_instanceId;
+    static AmazonSQS sqs_client;
+    static AmazonS3 s3_client;
+    static AWSCredentials credentials;
+
+    // queue urls: x_y_queue_url means x-->y direction queue
+    static String local_manager_queue_url;
+    static String manager_local_queue_url;
+    static String workers_manager_queue_url;
+    static String manager_workers_queue_url;
+
+    public static String worker_user_data;
+
+    public static String manager_user_data;
+
+
+
+    static void init() throws IOException {
+        System.out.println("init_credentials");
+        init_credentials();
+
+        System.out.println("init_s3");
+        init_s3();
+
+        System.out.println("init_ec2_client");
+        init_ec2_client();
+
+        System.out.println("init_sqs");
+        init_sqs();
+
+        worker_user_data = "#!/bin/bash" + "\n";
+        worker_user_data += "wget http://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.3.0/stanford-corenlp-3.3.0-models.jar && ";
+        worker_user_data += "wget http://repo1.maven.org/maven2/com/googlecode/efficient-java-matrix-library/ejml/0.23/ejml-0.23.jar && ";
+        worker_user_data += "wget http://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.3.0/stanford-corenlp-3.3.0.jar && ";
+        worker_user_data += "wget http://garr.dl.sourceforge.net/project/jollyday/releases/0.4.7/jollyday-0.4.7.jar && ";
+        worker_user_data += "wget http://malachi-amir-bucket.s3.amazonaws.com/worker.jar && java -jar worker.jar 5 >> worker_log.txt";
+        worker_user_data += "java -cp .:worker.jar:stanford-corenlp-3.3.0.jar:stanford-corenlp-3.3.0-models.jar:ejml-0.23.jar:jollyday-0.4.7.jar Analyzer";
+
+        manager_user_data = "#!/bin/bash" + "\n";
+        manager_user_data += "wget http://malachi-amir-bucket.s3.amazonaws.com/manager.jar && java -jar manager.jar 5 >> manager_log.txt";
+    }
+
+
+    private static void init_credentials() throws IOException {
+        credentials = new PropertiesCredentials(
+                Utils.class.getResourceAsStream("AwsCredentials.properties"));
+    }
+
+    private static void init_s3() {
+        s3_client = new AmazonS3Client(credentials);
+    }
+
+    private static void init_sqs() throws IOException {
+        // Create a queue
+        sqs_client = new AmazonSQSClient(credentials);
+
+        local_manager_queue_url = create_queue("local_manager_queue");
+        manager_local_queue_url = create_queue("manager_local_queue");
+        manager_workers_queue_url= create_queue("manager_workers_queue");
+        workers_manager_queue_url= create_queue("workers_manager_queue");
+//        Utils.clearAllSQS();
+//        System.out.println("DEBUG : ALL SQS CLEARED");
+    }
+
+
+    private static void init_ec2_client() throws IOException {
+        // Set client connection
+        ec2_client = new AmazonEC2Client(credentials);
+        ec2_client.setEndpoint("ec2.us-west-2.amazonaws.com");
+    }
+
+    /**
+     * create queue named give param name, if already exists, return the existing queue.
+     * @param name
+     * @return queue url named @name
+     */
+    private static String create_queue(String name) {
+        try {
+            CreateQueueRequest createQueueRequest = new CreateQueueRequest(name);
+            return sqs_client.createQueue(createQueueRequest).getQueueUrl();
+        } catch (Exception e) {
+            return sqs_client.getQueueUrl(name).getQueueUrl();
+        }
+    }
+
+
+    /**
+     *
+     * @param tag : tag of machine
+     * @param userData : user data for machine
+     * @return String instance ID of created machine
+     */
+    static String createEC2Instane(String tag, String userData) throws UnsupportedEncodingException {
+        // Request for booting machine up with key pair kp
+        RunInstancesRequest request = new RunInstancesRequest().
+                withImageId("ami-c229c0a2").
+                withMinCount(1).
+                withMaxCount(1).
+                withInstanceType(InstanceType.T2Micro).
+                withKeyName("kp").
+                withSecurityGroupIds("sg-01a8dd66");
+
+        // set user data in order to run whatever we want
+        String base64UserData = new String(Base64.encodeBase64(userData.getBytes("UTF-8")), "UTF-8");
+        request.setUserData(base64UserData);
+        RunInstancesResult runInstancesResult = Utils.ec2_client.runInstances(request);
+
+        // get the id of the created instance
+        String instancesId = runInstancesResult.getReservation().getInstances().get(0).getInstanceId();
+
+        // Tag the instance
+        tagInstance(instancesId, "name", tag);
+
+        return instancesId;
+    }
+
+    /**
+     * Giving name (=tag) to an instance, in order to know if it is a manager / worker
+     *
+     * @param instanceId
+     * the instance to tag
+     *
+     * @param tag
+     * the tag to give to the instance ( example: "name" )
+     *
+     * @param value
+     * the value of the tag ( example: "worker" )
+     */
+    //
+    public static void tagInstance(String instanceId, String tag, String value) {
+        CreateTagsRequest request = new CreateTagsRequest();
+        request = request.withResources(instanceId)
+                .withTags(new Tag(tag, value));
+        Utils.ec2_client.createTags(request);
+    }
+
+    /**
+     * Clear queue for debugging..
+     */
+    public static void clearSQS(String queueUrl){
+        sqs_client.purgeQueue(new PurgeQueueRequest(queueUrl));
+
+    }
+
+    public static void clearAllSQS(){
+        Utils.clearSQS(Utils.manager_local_queue_url);
+        Utils.clearSQS(Utils.local_manager_queue_url);
+        Utils.clearSQS(Utils.manager_workers_queue_url);
+        Utils.clearSQS(Utils.workers_manager_queue_url);
+    }
+
+    public static String createWorker() throws UnsupportedEncodingException {
+        return createEC2Instane("worker", worker_user_data);
+    }
+
+    public static String createManager() throws UnsupportedEncodingException {
+        return createEC2Instane("manager", manager_user_data);
+    }
+}
